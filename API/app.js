1bda43de2c917145059af621ab8cd19c47dc7e2a
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2026-03-24 14:05:16 +0100

    feat(webapp): created dockerfile and webapp service in dockercompose

--- API/app.js
+++ API/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 const postsRoutes = require("./routes/posts");
 const pseudonymsRoutes = require("./routes/pseudonyms");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
